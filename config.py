7b4a0655684c2d9e641c95f7452df7b0da648496
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-19 19:13:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,34 +1,64 @@
 BOT_TOKEN ="8920872994:AAEroNdwptEOqUb1ercwdLqRI1ATlGALNUE"
+ALPHA_VANTAGE_API_KEY = "CR46XFILZJ2NH4KP"
 
 LANGUAGES = ["ar", "en"]
 
 PAIRS = [
+
     "EUR/USD",
+
     "GBP/USD",
+
     "USD/JPY",
+
     "USD/CHF",
+
     "USD/CAD",
+
     "AUD/USD",
+
     "NZD/USD",
+
     "EUR/JPY",
+
     "GBP/JPY",
+
     "EUR/GBP",
+
     "EUR/CHF",
+
     "EUR/CAD",
+
     "EUR/AUD",
+
     "GBP/AUD",
+
     "GBP/CAD",
+
     "GBP/CHF",
+
     "AUD/JPY",
+
     "AUD/CAD",
+
     "AUD/CHF",
+
     "CAD/JPY",
+
     "CHF/JPY",
+
     "NZD/JPY",
+
     "NZD/CAD",
+
     "NZD/CHF",
+
     "EUR/NZD",
+
     "GBP/NZD",
+
     "XAU/USD",
+
     "XAG/USD"
+
 ]
