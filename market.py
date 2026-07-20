346e39573b8a482069cdb9fc53aab3854f2caec3
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-20 22:53:51 +0300

    Update market.py

--- market.py
+++ market.py
@@ -2,21 +2,24 @@ import requests
 from config import TWELVE_API
 
 
-def get_price(pair):
+def get_candles(pair):
     try:
         url = (
-            f"https://api.twelvedata.com/price"
+            f"https://api.twelvedata.com/time_series"
             f"?symbol={pair}"
+            f"&interval=1min"
+            f"&outputsize=100"
             f"&apikey={TWELVE_API}"
         )
 
         response = requests.get(url, timeout=10)
         data = response.json()
 
-        if "price" in data:
-            return float(data["price"])
+        if "values" not in data:
+            print(data)
+            return None
 
-        return None
+        return data["values"]
 
     except Exception as e:
         print(e)
