a89078a3072d2ccc2b27f7c59b8c44fa910afce9
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-20 10:23:48 +0300

    Update market.py

--- market.py
+++ market.py
@@ -1,42 +1,25 @@
 import requests
-
-API_KEY = "B3ZU4ZS44965VIYO"
+from config import TWELVE_API
 
 def get_price(pair):
     try:
         pair = pair.replace(" (ذهب)", "")
 
-        # الذهب
-        if pair == "XAU/USD":
-            url = (
-                f"https://www.alphavantage.co/query"
-                f"?function=CURRENCY_EXCHANGE_RATE"
-                f"&from_currency=XAU"
-                f"&to_currency=USD"
-                f"&apikey={API_KEY}"
-            )
-        else:
-            base, quote = pair.split("/")
-
-            url = (
-                f"https://www.alphavantage.co/query"
-                f"?function=CURRENCY_EXCHANGE_RATE"
-                f"&from_currency={base}"
-                f"&to_currency={quote}"
-                f"&apikey={API_KEY}"
-            )
+        url = (
+            f"https://api.twelvedata.com/price"
+            f"?symbol={pair}"
+            f"&apikey={TWELVE_API}"
+        )
 
         response = requests.get(url, timeout=10)
         data = response.json()
 
-        if "Realtime Currency Exchange Rate" not in data:
-            print(data)
-            return None
+        if "price" in data:
+            return round(float(data["price"]), 5)
 
-        price = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
-
-        return round(float(price), 5)
+        print(data)
+        return None
 
     except Exception as e:
-        print("Market Error:", e)
+        print(e)
         return None
