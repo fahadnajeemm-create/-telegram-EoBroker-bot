def733f51f3fbaafa355ca81f783fabc31d55a67
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-20 15:08:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,14 +59,15 @@ keyboard = types.InlineKeyboardMarkup()
 for pair in pairs:
 keyboard.add( types.InlineKeyboardButton( pair,
 callback_data=f"pair_{pair}"))
-bot.send_message(chat_id,    "اختر الزوج:",
+bot.send_message(chat_id,"اختر الزوج:",
 reply_markup=keyboard ) 
 elif call.data.startswith("pair_"):
 pair = call.data.replace("pair_", "")
 user_pair[chat_id] = pair
-bot.send_message(chat_id,f"تم اختيار الزوج ✅\n{pair}")
+bot.send_message(chat_id, f"تم اختيار الزوج ✅\n{pair}")
 main_menu(chat_id)
-elif call.data == "signal":
+elif 
+call.data == "signal":
 pair = user_pair.get(chat_id, "EUR/USD")
 price = get_price(pair)
 if price:
