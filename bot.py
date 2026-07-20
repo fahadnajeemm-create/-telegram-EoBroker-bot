6cf894bd4e4de3b3172e65cbf55852bde76aa200
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-20 16:21:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 from market import get_price 
 from database import (
-    add_subscription,
-    remove_subscription,
-    check_subscription,
-    days_left,
-    all_users
+add_subscription,
+remove_subscription,
+check_subscription,
+ days_left,
+ all_users
 )
 TOKEN = "8920872994:AAG0t2VC48sfLIBznsjn9OUEV6A5VpKgnlc"
 ADMIN_ID = 123456789
