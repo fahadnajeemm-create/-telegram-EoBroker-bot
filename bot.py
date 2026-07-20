664031880483c8b7fe262134e055dd5cc0dd08a9
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-20 15:13:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,21 +54,18 @@ bot.send_message(chat_id, "تم اختيار العربية ✅")
  else:
 bot.send_message(chat_id, "English selected ✅")
 main_menu(chat_id)
-elif 
-call.data == "pairs":
+elif call.data == "pairs":
 keyboard = types.InlineKeyboardMarkup()
 for pair in pairs:
 keyboard.add( types.InlineKeyboardButton( pair,
 callback_data=f"pair_{pair}"))
 bot.send_message(chat_id,"اختر الزوج:", reply_markup=keyboard ) 
-elif 
-call.data.startswith("pair_"):
+elif call.data.startswith("pair_"):
 pair = call.data.replace("pair_", "")
 user_pair[chat_id] = pair
 bot.send_message(chat_id, f"تم اختيار الزوج ✅\n{pair}")
 main_menu(chat_id)
-elif 
-call.data == "signal":
+elif call.data == "signal":
 pair = user_pair.get(chat_id, "EUR/USD")
 price = get_price(pair)
 if price:
@@ -78,12 +75,10 @@ if price > old_price:
 signal = "🟢 شراء (CALL)"
 elif price < old_price:
 signal = "🔴 بيع (PUT)"
-else:
-signal = "⏸ انتظار"
+else: signal = "⏸ انتظار"
 if price:
 signal = "✅ تم جلب السعر"
-else:
-signal = "⏳ جمع البيانات..."
+else: signal = "⏳ جمع البيانات..."
 last_prices[pair] = price
 bot.send_message(chat_id,
 f"💱 الزوج: {pair}\n"
@@ -91,7 +86,6 @@ f"💰 السعر الحالي: {price}\n"
  f"📊 الإشارة: {signal}\n"
  f"⏱ مدة الصفقة: 30 ثانية\n"
     f"⏰ الوقت: {datetime.now(ZoneInfo('Asia/Riyadh')).strftime('%H:%M')}"  )
-else:
-bot.send_message(chat_id,f"❌ لم يتم جلب السعر للزوج {pair}"(
+else:bot.send_message(chat_id,f"❌ لم يتم جلب السعر للزوج {pair}"(
 print("Bot is running...")
 bot.infinity_polling()
