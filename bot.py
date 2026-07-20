96131a2c343b1f0959ba790307468ed307541fa5
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-20 09:23:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telebot import types
 import random
 from datetime import datetime
 from market import get_price
-TOKEN = "8976870107:AAH46xglYTK5i9LEsfbtivI32RT5xwsgke0"
+TOKEN = "8920872994:AAG0t2VC48sfLIBznsjn9OUEV6A5VpKgnlc"
 
 bot = telebot.TeleBot(TOKEN)
 
