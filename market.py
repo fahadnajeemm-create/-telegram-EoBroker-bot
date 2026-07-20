d2cb83639c5480f1c57bb1963dc0d35d2808482a
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-20 20:26:19 +0300

    Update market.py

--- market.py
+++ market.py
@@ -1,13 +1,7 @@
 import requests
-
 import pandas as pd
-
 import pandas_ta as ta
-
 from config import TWELVE_API
-
 def get_price(pair):
-
-    try:
-
-        pair = pair.replace(" (ذهب)", "")
+try:
+pair = pair.replace(" (ذهب)", "")
