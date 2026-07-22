cbe01c04a72ef9ebe2b1cc51e677fe5cfec1b1fb
fahadnajeemm-create <fahad.najeemm@gmail.com>
2026-07-22 18:43:09 +0300

    Update market.py

--- market.py
+++ market.py
@@ -114,6 +114,7 @@ def analyze_market(pair):
         bb = ta.volatility.BollingerBands(close=df["close"], window=20, window_dev=2)
         df["bb_high"] = bb.bollinger_hband()
         df["bb_low"] = bb.bollinger_lband()
+        df["bb_mid"] = bb.bollinger_mavg() 
         
         df = df.dropna()
         if len(df) == 0:
